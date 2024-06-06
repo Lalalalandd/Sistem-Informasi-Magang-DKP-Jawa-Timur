e4a1581273e3b5d9293abeb3db67b982af10241c
Jefri Achmad Maulana <jefriachmadm@gmail.com>
2024-06-06 18:59:37 +0700

    add view

---
+++ resources/views/pegawai/magang_pegawai.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.template')
+@section('content')
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    
+                </div>
+        
+
+
+            </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+</div>
+@endsection

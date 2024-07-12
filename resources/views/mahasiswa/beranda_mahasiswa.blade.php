8de789d5456fda380c71b889a41afbac217fa4d8
Jefri Achmad Maulana <jefriachmadm@gmail.com>
2024-07-12 20:03:39 +0700

    fix carousel

--- resources/views/mahasiswa/beranda_mahasiswa.blade.php
+++ resources/views/mahasiswa/beranda_mahasiswa.blade.php
@@ -112,58 +112,51 @@
                     <!-- /.col -->
 
                     <div class="col-md-6">
-                        <div class="card card-info">
-                            <div class="card-header">
-                                <h3 class="card-title">
-                                    <i class=""><svg xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em"
-                                            viewBox="0 0 256 256">
-                                            <path fill="currentColor"
-                                                d="M128 24a104 104 0 1 0 104 104A104.11 104.11 0 0 0 128 24m-4 48a12 12 0 1 1-12 12a12 12 0 0 1 12-12m12 112a16 16 0 0 1-16-16v-40a8 8 0 0 1 0-16a16 16 0 0 1 16 16v40a8 8 0 0 1 0 16" />
-                                        </svg></i>
-                                    Info
-                                </h3>
-                            </div>
+                        <div class="card card-default" style="height: 276px;">
                             <!-- /.card-header -->
-                            <div class="card-body">
-                                <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-                                    <ol class="carousel-indicators">
-                                        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                                        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                                        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                                        <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
-                                    </ol>
-                                    <div class="carousel-inner">
-                                        <div class="carousel-item active">
-                                            <img class="d-block w-100" src="template/img/slider-141.jpg" height="201px"
-                                                alt="First slide">
+                            <div class="card-body ">
+                                <div class="">
+                                    <div id="carouselExampleIndicators" class="carousel slide mt-3" data-ride="carousel">
+                                        <ol class="carousel-indicators">
+                                            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active">
+                                            </li>
+                                            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                                            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                                            <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+                                        </ol>
+                                        <div class="carousel-inner">
+                                            <div class="carousel-item active">
+                                                <img class="d-block w-100" src="template/img/slider-141.jpg" height="201px"
+                                                    alt="First slide">
+                                            </div>
+                                            <div class="carousel-item">
+                                                <img class="d-block w-100" src="template/img/slider-142.jpg" height="201px"
+                                                    alt="Second slide">
+                                            </div>
+                                            <div class="carousel-item">
+                                                <img class="d-block w-100" src="template/img/slider-143.jpg" height="201px"
+                                                    alt="Third slide">
+                                            </div>
+                                            <div class="carousel-item">
+                                                <img class="d-block w-100" src="template/img/slider-144.jpg" height="201px"
+                                                    alt="Fourth slide">
+                                            </div>
                                         </div>
-                                        <div class="carousel-item">
-                                            <img class="d-block w-100" src="template/img/slider-142.jpg" height="201px"
-                                                alt="Second slide">
-                                        </div>
-                                        <div class="carousel-item">
-                                            <img class="d-block w-100" src="template/img/slider-143.jpg" height="201px"
-                                                alt="Third slide">
-                                        </div>
-                                        <div class="carousel-item">
-                                            <img class="d-block w-100" src="template/img/slider-144.jpg" height="201px"
-                                                alt="Fourth slide">
-                                        </div>
+                                        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button"
+                                            data-slide="prev">
+                                            <span class="carousel-control-custom-icon" aria-hidden="true">
+                                                <i class="fas fa-chevron-left"></i>
+                                            </span>
+                                            <span class="sr-only">Previous</span>
+                                        </a>
+                                        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button"
+                                            data-slide="next">
+                                            <span class="carousel-control-custom-icon" aria-hidden="true">
+                                                <i class="fas fa-chevron-right"></i>
+                                            </span>
+                                            <span class="sr-only">Next</span>
+                                        </a>
                                     </div>
-                                    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button"
-                                        data-slide="prev">
-                                        <span class="carousel-control-custom-icon" aria-hidden="true">
-                                            <i class="fas fa-chevron-left"></i>
-                                        </span>
-                                        <span class="sr-only">Previous</span>
-                                    </a>
-                                    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button"
-                                        data-slide="next">
-                                        <span class="carousel-control-custom-icon" aria-hidden="true">
-                                            <i class="fas fa-chevron-right"></i>
-                                        </span>
-                                        <span class="sr-only">Next</span>
-                                    </a>
                                 </div>
 
                             </div>
